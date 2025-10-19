491d35a91be4547eac28d8c9c05b165935d59a29
Gilbert Ramirez <gram@alumni.rice.edu>
2025-10-19 05:57:04 -0500

    Unit tests for directions.go pass

---
+++ directions_test.go
@@ -0,0 +1,49 @@
+// Copyright (c) 2025 by Gilbert Ramirez <gram@alumni.rice.edu>.
+// All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package wosecon
+
+import (
+	. "github.com/gilramir/gocheck-extra"
+	. "gopkg.in/check.v1"
+)
+
+func (s *MySuite) TestDirectionsNaturalLTR(c *C) {
+	var d Direction = NaturalLTRDirections
+
+	directions := getIndividualDirections(d)
+	c.Assert(len(directions), Equals, 4)
+
+	// Has these
+	c.Check(Down, InSlice, directions)
+	c.Check(LTRHorizontal, InSlice, directions)
+	c.Check(LTRDescending, InSlice, directions)
+	c.Check(LTRAscending, InSlice, directions)
+
+	// Does not have these
+	c.Check(Up, Not(InSlice), directions)
+	c.Check(RTLHorizontal, Not(InSlice), directions)
+	c.Check(RTLDescending, Not(InSlice), directions)
+	c.Check(RTLAscending, Not(InSlice), directions)
+}
+
+func (s *MySuite) TestDirectionsAll(c *C) {
+	var d Direction = AllDirections
+
+	directions := getIndividualDirections(d)
+	c.Assert(len(directions), Equals, 8)
+
+	// Has these
+	c.Check(Down, InSlice, directions)
+	c.Check(LTRHorizontal, InSlice, directions)
+	c.Check(LTRDescending, InSlice, directions)
+	c.Check(LTRAscending, InSlice, directions)
+
+	// And these too
+	c.Check(Up, InSlice, directions)
+	c.Check(RTLHorizontal, InSlice, directions)
+	c.Check(RTLDescending, InSlice, directions)
+	c.Check(RTLAscending, InSlice, directions)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/gilramir/golang-WoSeCon
 go 1.25.1
 
 require (
+	github.com/gilramir/gocheck-extra v1.0.0 // indirect
 	github.com/kr/pretty v0.2.1 // indirect
 	github.com/kr/text v0.1.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
