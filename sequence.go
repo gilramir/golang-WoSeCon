d32d1aa6a5abfb4bf6c85871c1e18a1b81742085
Gilbert Ramirez <gram@alumni.rice.edu>
2025-11-09 09:49:01 -0600

    Add StringSequence

--- sequence.go
+++ sequence.go
@@ -2,6 +2,8 @@ package wosecon
 
 import (
 	"iter"
+	"slices"
+	"strings"
 )
 
 type Sequence interface {
@@ -13,7 +15,8 @@ type Sequence interface {
 }
 
 // RuneSequence is used for the most
-// common type of word search; it's for strings
+// common type of word search; it's for strings where each
+// rune in the string is one cell in the puzzle
 type RuneSequence struct {
 	runes []rune
 }
@@ -55,6 +58,58 @@ func (s RuneSequence) Items() iter.Seq2[int, string] {
 	return func(yield func(int, string) bool) {
 		for i, r := range s.runes {
 			if !yield(i, string(r)) {
+				return
+			}
+		}
+	}
+}
+
+// StringSequence is a special case. It's for strings
+// where more than one rune in the string is one cell in
+// the puzzle. It's useful if you're creating a word search
+// puzzle for a language like Thai, where a single written "letter"
+// might have 1, 2, or 3 runes combined together.
+type StringSequence struct {
+	parts []string
+}
+
+func NewStringSequence(parts []string) StringSequence {
+	return StringSequence{
+		parts: slices.Clone(parts),
+	}
+}
+
+func (s StringSequence) String() string {
+	return strings.Join(s.parts, "")
+}
+
+func (s StringSequence) Len() int {
+	return len(s.parts)
+}
+
+func (s StringSequence) Cmp(other Sequence) int {
+	m := s.String()
+	o := other.String()
+	if m < o {
+		return -1
+	} else if m > o {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func (s StringSequence) Index(n int) string {
+	if n < len(s.parts) {
+		return string(s.parts[n])
+	} else {
+		return ""
+	}
+}
+func (s StringSequence) Items() iter.Seq2[int, string] {
+	return func(yield func(int, string) bool) {
+		for i, p := range s.parts {
+			if !yield(i, p) {
 				return
 			}
 		}
